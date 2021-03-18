868724214f053c0b3e29dd75eb708c4edcdde4a3
Tatu Tolonen <tjtolon@gmail.com>
2021-03-19 00:24:09 +0200

    works with newer vcpkg

--- apps/dashboard/main.cpp
+++ apps/dashboard/main.cpp
@@ -4,169 +4,177 @@
 #include <Magnum/GL/Renderer.h>
 #include <Magnum/Platform/Sdl2Application.h>
 #include <Magnum/Math/Complex.h>
+#include <Magnum/Math/Color.h>
+#include <Magnum/Math/Matrix3.h>
 #include <Magnum/Shaders/VertexColor.h>
 #include <Magnum/Shaders/DistanceFieldVector.h>
 #include <Magnum/Text/AbstractFont.h>
 #include <Magnum/Text/DistanceFieldGlyphCache.h>
 #include <Magnum/Text/Renderer.h>
 #include <Corrade/Utility/Format.h>
+#include <Corrade/Utility/Resource.h>
 #include <Corrade/PluginManager/Manager.h>
+#include <fmt/format.h>
 
 #include <memory>
 
 namespace Magnum::Examples {
-using namespace Math::Literals;
+    using namespace Math::Literals;
 
-class TriangleExample : public Platform::Application {
- public:
-  explicit TriangleExample(const Arguments& arguments);
+    class TriangleExample : public Platform::Application {
+    public:
+        explicit TriangleExample(const Arguments& arguments);
 
- private:
-  void viewportEvent(ViewportEvent& event) override;
-  void drawEvent() override;
-  void updateText();
-  void mouseScrollEvent(MouseScrollEvent& event) override;
+    private:
+        void viewportEvent(ViewportEvent& event) override;
 
-  GL::Mesh _mesh;
-  Shaders::VertexColor2D _shader;
-  Shaders::DistanceFieldVector2D _text_shader;
+        void drawEvent() override;
 
-  PluginManager::Manager<Text::AbstractFont> _manager;
-  std::unique_ptr<Text::AbstractFont> _font;
-  std::unique_ptr<Text::Renderer2D> _dynamicText;
-  Text::DistanceFieldGlyphCache _cache;
-  GL::Buffer _vertices, _indices;
-  GL::Mesh _text;
+        void updateText();
 
-  Matrix3 _transformationRotatingText,
-      _projectionRotatingText,
-      _transformationProjectionDynamicText;
-};
+        void mouseScrollEvent(MouseScrollEvent& event) override;
 
-TriangleExample::TriangleExample(const Arguments& arguments) :
-    Platform::Application{arguments, Configuration{}.setTitle("Magnum Triangle Example")},
-    _cache(Vector2i(2048), Vector2i(512), 22) {
+        GL::Mesh _mesh;
+        Shaders::VertexColor2D _shader;
+        Shaders::DistanceFieldVector2D _text_shader;
 
-  _font = _manager.loadAndInstantiate("FreeTypeFont");
-  if (!_font) std::exit(1);
+        PluginManager::Manager<Text::AbstractFont> _manager;
+        std::unique_ptr<Text::AbstractFont> _font;
+        std::unique_ptr<Text::Renderer2D> _dynamicText;
+        Text::DistanceFieldGlyphCache _cache;
+        GL::Buffer _vertices, _indices;
+        GL::Mesh _text;
 
-  /* Open the font and fill glyph cache */
-  Utility::Resource rs("fonts");
-  auto dejavu = rs.getRaw("DejaVuSans.ttf");
-  if (!_font->openData({{"DejaVuSans.ttf", dejavu}}, 110.0f)) {
-    Error() << "Cannot open font file";
-    std::exit(1);
-  }
-  _font->fillGlyphCache(_cache,
-                        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789:-+,.!°ěäЗдравстуймиΓειασουκόμ ");
-  std::tie(_text, std::ignore) = Text::Renderer2D::render(
-      *_font, _cache, 0.1295f,
-      "Hello, world!\n"
-      "Ahoj, světe!\n"
-      "Здравствуй, мир!\n"
-      "Γεια σου κόσμε!\n"
-      "Hej Världen!",
-      _vertices, _indices, GL::BufferUsage::StaticDraw, Text::Alignment::MiddleCenter);
+        Matrix3 _transformationRotatingText,
+                _projectionRotatingText,
+                _transformationProjectionDynamicText;
+    };
 
-  _transformationRotatingText = Matrix3::rotation(-10.0_degf);
-  _projectionRotatingText = Matrix3::projection(
-      Vector2::xScale(Vector2{windowSize()}.aspectRatio()));
+    TriangleExample::TriangleExample(const Arguments& arguments) :
+            Platform::Application{arguments, Configuration{}.setTitle("Magnum Triangle Example")},
+            _cache(Vector2i(2048), Vector2i(512), 22) {
 
-  /* Dynamically updated text that shows rotation/zoom of the other. Size in
-     points that stays the same if you resize the window. Aligned so top
-     right of the bounding box is at mesh origin, and then transformed so the
-     origin is at the top right corner of the window. */
-  _dynamicText = std::make_unique<Text::Renderer2D>(*_font, _cache, 32.0f, Text::Alignment::TopRight);
-  _dynamicText->reserve(40, GL::BufferUsage::DynamicDraw, GL::BufferUsage::StaticDraw);
-  _transformationProjectionDynamicText =
-      Matrix3::projection(Vector2{windowSize()}) *
-          Matrix3::translation(Vector2{windowSize()} * 0.5f);
+        _font = _manager.loadAndInstantiate("FreeTypeFont");
+        if (!_font) std::exit(1);
 
-  /* Set up premultiplied alpha blending to avoid overlapping text characters
-     to cut into each other */
-  GL::Renderer::enable(GL::Renderer::Feature::Blending);
-  GL::Renderer::setBlendFunction(GL::Renderer::BlendFunction::One, GL::Renderer::BlendFunction::OneMinusSourceAlpha);
-  GL::Renderer::setBlendEquation(GL::Renderer::BlendEquation::Add, GL::Renderer::BlendEquation::Add);
+        /* Open the font and fill glyph cache */
+        Utility::Resource rs("fonts");
+        auto dejavu = rs.getRaw("DejaVuSans.ttf");
+        if (!_font->openData({{"DejaVuSans.ttf", dejavu}}, 110.0f)) {
+            Error() << "Cannot open font file";
+            std::exit(1);
+        }
+        _font->fillGlyphCache(_cache,
+                              "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789:-+,.!°ěäЗдравстуймиΓειασουκόμ ");
+        std::tie(_text, std::ignore) = Text::Renderer2D::render(
+                *_font, _cache, 0.1295f,
+                "Hello, world!\n"
+                "Ahoj, světe!\n"
+                "Здравствуй, мир!\n"
+                "Γεια σου κόσμε!\n"
+                "Hej Världen!",
+                _vertices, _indices, GL::BufferUsage::StaticDraw, Text::Alignment::MiddleCenter);
 
-  updateText();
+        _transformationRotatingText = Matrix3::rotation(-10.0_degf);
+        _projectionRotatingText = Matrix3::projection(
+                Vector2::xScale(Vector2{windowSize()}.aspectRatio()));
 
-  struct TriangleVertex {
-    Vector2 position;
-    Color3 color;
-  };
-  const TriangleVertex data[]{
-      {{-0.5f, -0.5f}, 0xff0000_rgbf},    /* Left vertex, red color */
-      {{0.5f, -0.5f}, 0x00ff00_rgbf},    /* Right vertex, green color */
-      {{0.0f, 0.5f}, 0x0000ff_rgbf}     /* Top vertex, blue color */
-  };
+        /* Dynamically updated text that shows rotation/zoom of the other. Size in
+           points that stays the same if you resize the window. Aligned so top
+           right of the bounding box is at mesh origin, and then transformed so the
+           origin is at the top right corner of the window. */
+        _dynamicText = std::make_unique<Text::Renderer2D>(*_font, _cache, 32.0f, Text::Alignment::TopRight);
+        _dynamicText->reserve(40, GL::BufferUsage::DynamicDraw, GL::BufferUsage::StaticDraw);
+        _transformationProjectionDynamicText =
+                Matrix3::projection(Vector2{windowSize()}) *
+                Matrix3::translation(Vector2{windowSize()} * 0.5f);
 
-  GL::Buffer buffer;
-  buffer.setData(data);
+        /* Set up premultiplied alpha blending to avoid overlapping text characters
+           to cut into each other */
+        GL::Renderer::enable(GL::Renderer::Feature::Blending);
+        GL::Renderer::setBlendFunction(GL::Renderer::BlendFunction::One,
+                                       GL::Renderer::BlendFunction::OneMinusSourceAlpha);
+        GL::Renderer::setBlendEquation(GL::Renderer::BlendEquation::Add, GL::Renderer::BlendEquation::Add);
 
-  _mesh.setCount(3)
-      .addVertexBuffer(std::move(buffer), 0,
-                       Shaders::VertexColor2D::Position{},
-                       Shaders::VertexColor2D::Color3{});
-}
+        updateText();
 
-void TriangleExample::drawEvent() {
-  GL::defaultFramebuffer.clear(GL::FramebufferClear::Color);
+        struct TriangleVertex {
+            Vector2 position;
+            Color3 color;
+        };
+        const TriangleVertex data[]{
+                {{-0.5f, -0.5f}, 0xff0000_rgbf},    /* Left vertex, red color */
+                {{0.5f,  -0.5f}, 0x00ff00_rgbf},    /* Right vertex, green color */
+                {{0.0f,  0.5f},  0x0000ff_rgbf}     /* Top vertex, blue color */
+        };
 
-  _mesh.draw(_shader);
+        GL::Buffer buffer;
+        buffer.setData(data);
 
-  _text_shader.bindVectorTexture(_cache.texture());
+        _mesh.setCount(3)
+                .addVertexBuffer(std::move(buffer), 0,
+                                 Shaders::VertexColor2D::Position{},
+                                 Shaders::VertexColor2D::Color3{});
+    }
 
-  _text_shader
-      .setTransformationProjectionMatrix(
-          _projectionRotatingText * _transformationRotatingText)
-      .setColor(0x2f83cc_rgbf)
-      .setOutlineColor(0xdcdcdc_rgbf)
-      .setOutlineRange(0.45f, 0.35f)
-      .setSmoothness(0.025f / _transformationRotatingText.uniformScaling());
-  _text.draw(_text_shader);
+    void TriangleExample::drawEvent() {
+        GL::defaultFramebuffer.clear(GL::FramebufferClear::Color);
 
-  _text_shader
-      .setTransformationProjectionMatrix(_transformationProjectionDynamicText)
-      .setColor(0xffffff_rgbf)
-      .setOutlineRange(0.5f, 1.0f)
-      .setSmoothness(0.075f);
-  _dynamicText->mesh().draw(_text_shader);
+        _mesh.draw(_shader);
 
-  swapBuffers();
-}
+        _text_shader.bindVectorTexture(_cache.texture());
 
-void TriangleExample::mouseScrollEvent(MouseScrollEvent& event) {
-  if (!event.offset().y()) return;
+        _text_shader
+                .setTransformationProjectionMatrix(
+                        _projectionRotatingText * _transformationRotatingText)
+                .setColor(0x2f83cc_rgbf)
+                .setOutlineColor(0xdcdcdc_rgbf)
+                .setOutlineRange(0.45f, 0.35f)
+                .setSmoothness(0.025f / _transformationRotatingText.uniformScaling());
+        _text.draw(_text_shader);
 
-  if (event.offset().y() > 0)
-    _transformationRotatingText =
-        Matrix3::rotation(1.0_degf) * Matrix3::scaling(Vector2{1.1f}) * _transformationRotatingText;
-  else
-    _transformationRotatingText =
-        Matrix3::rotation(-1.0_degf) * Matrix3::scaling(Vector2{1.0f / 1.1f}) * _transformationRotatingText;
+        _text_shader
+                .setTransformationProjectionMatrix(_transformationProjectionDynamicText)
+                .setColor(0xffffff_rgbf)
+                .setOutlineRange(0.5f, 1.0f)
+                .setSmoothness(0.075f);
+        _dynamicText->mesh().draw(_text_shader);
 
-  updateText();
+        swapBuffers();
+    }
 
-  event.setAccepted();
-  redraw();
-}
+    void TriangleExample::mouseScrollEvent(MouseScrollEvent& event) {
+        if (!event.offset().y()) return;
 
-void TriangleExample::updateText() {
-  _dynamicText->render(Utility::formatString("Rotation: {:.2}°\nScale: {:.2}",
-                                             Float(Deg(Magnum::Math::Complex<float>::fromMatrix(
-                                                 _transformationRotatingText.rotation()).angle())),
-                                             _transformationRotatingText.uniformScaling()));
-}
+        if (event.offset().y() > 0)
+            _transformationRotatingText =
+                    Matrix3::rotation(1.0_degf) * Matrix3::scaling(Vector2{1.1f}) * _transformationRotatingText;
+        else
+            _transformationRotatingText =
+                    Matrix3::rotation(-1.0_degf) * Matrix3::scaling(Vector2{1.0f / 1.1f}) * _transformationRotatingText;
 
-void TriangleExample::viewportEvent(ViewportEvent& event) {
-  GL::defaultFramebuffer.setViewport({{}, event.framebufferSize()});
+        updateText();
 
-  _projectionRotatingText = Matrix3::projection(
-      Vector2::xScale(Vector2{windowSize()}.aspectRatio()));
-  _transformationProjectionDynamicText =
-      Matrix3::projection(Vector2{windowSize()}) *
-          Matrix3::translation(Vector2{windowSize()} * 0.5f);
-}
+        event.setAccepted();
+        redraw();
+    }
+
+    void TriangleExample::updateText() {
+        _dynamicText->render(fmt::format("Rotation: {:.2}°\nScale: {:.2}",
+                                                   Float(Deg(Magnum::Math::Complex<float>::fromMatrix(
+                                                           _transformationRotatingText.rotation()).angle())),
+                                                   _transformationRotatingText.uniformScaling()));
+    }
+
+    void TriangleExample::viewportEvent(ViewportEvent& event) {
+        GL::defaultFramebuffer.setViewport({{}, event.framebufferSize()});
+
+        _projectionRotatingText = Matrix3::projection(
+                Vector2::xScale(Vector2{windowSize()}.aspectRatio()));
+        _transformationProjectionDynamicText =
+                Matrix3::projection(Vector2{windowSize()}) *
+                Matrix3::translation(Vector2{windowSize()} * 0.5f);
+    }
 
 }
 
--- scripts/install-windows/vcpkg.ps1
+++ scripts/install-windows/vcpkg.ps1
@@ -7,4 +7,4 @@ if (-not (Test-Path -Path $vcpkg_dir)) {
     git clone https://github.com/microsoft/vcpkg.git
     Invoke-Expresssion "$vcpkg_dir\bootstrap-vcpkg.bat"
 }
-Invoke-Expression "$vcpkg_dir\vcpkg.exe --triplet x64-windows-static install spdlog fmt corrade magnum[*] boost magnum-plugins magnum-extras corrade"
+Invoke-Expression "$vcpkg_dir\vcpkg.exe --triplet x64-windows-static install spdlog fmt corrade magnum boost magnum-plugins[freetypefont] magnum-extras[ui] corrade freetype"
